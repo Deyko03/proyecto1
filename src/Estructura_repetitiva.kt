151d2491f6be4cd6d61c425073dd545a6070da71
Deyko03 <deykerlanzavargas395mail.com>
2024-06-10 23:54:45 -0600

    Hice los problemas propuestos en casa

--- src/Estructura_repetitiva.kt
+++ src/Estructura_repetitiva.kt
@@ -30,7 +30,7 @@ fun main () {
         val promedio = suma / cant
         print("El promedio de los valores ingresados es: $promedio")
     }*/
-
+    /*
     var cantidad_inferior = 0 // por debajo de 9.8
     var cantidad_superior = 0 //por debajo de 10.2
     var cantidad_adecuada = 0 // entre 9.8 y 10.2
@@ -50,5 +50,58 @@ fun main () {
     println("Piezas con un peso inferior a 9.8: $cantidad_inferior")
     val suma = cantidad_inferior + cantidad_adecuada + cantidad_superior
     println("Cantidad total de piezas procesadas: $suma")
+*/
+    //Problemas propuestos
+/*
+    var valorAcumulado = 0
+    var entrada: Int
 
+    println("Ingrese los valores a sumar. Ingrese 9999 para finalizar.")
+
+    do {
+        print("Ingrese un valor: ")
+        entrada = readln().toInt()
+
+        if (entrada != 9999) {
+            valorAcumulado += entrada
+        }
+    } while (entrada != 9999)
+
+    println("El valor acumulado es: $valorAcumulado")
+
+    if(valorAcumulado == 0) {
+        println("El valor acumulado es cero.")
+     (valorAcumulado > 0)
+        println("El valor acumulado es mayor a cero.")
+    }else{
+        println("El valor acumulado es menor a cero.")
+    }*/
+
+    var saldos = 0
+    var cuenta: Int
+    var saldo: Double
+
+    println("Ingrese los datos de las cuentas corrientes. Ingrese un número de cuenta negativo para finalizar.")
+
+    do {
+        print("Ingrese el número de cuenta: ")
+        cuenta = readLine()!!.toInt()
+
+        if (cuenta >= 0) {
+            print("Ingrese el saldo actual: ")
+            saldo = readLine()!!.toDouble()
+
+            (saldo > 0)
+            println("Estado de la cuenta: Acreedor")
+            saldos += saldo.toInt()
+
+            (saldo < 0)
+            println("Estado de la cuenta: Deudor")
+        }else{
+            println("Estado de la cuenta: Nulo")
+
+        }
+    } while (cuenta >= 0)
+
+    println("La suma total de los saldos acreedores es: $saldos")
 }
